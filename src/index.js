77fda6e1cb4f909355990a0558fc5cc7c1ab2ad5
Ryan Van Etten <ryanve@gmail.com>
2013-11-17 01:29:07 -0500

    Remove excess whitespace from annoations.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@
      * Logic for discerning arrays/arr-like object from other objects or types.
      * If `o` is a valid "object" w/ a non-NaN "number" length prop, it returns 
      * the length. Otherwise it returns false.
-     * @param  {*}  o  is the object (or unknown) in question
+     * @param {*} o value in question
      * @return {number|boolean}
      */
     function count(o) {
@@ -22,8 +22,8 @@
     
     /**
      * Push an item (or list) into a stack and return the stack.
-     * @param  {Object|Array} target  receiver (becomes array-like if not already)
-     * @param  {*=} item    value (or list) to push into `stack`
+     * @param {{length:number}} target receiver (becomes array-like if not already)
+     * @param {*=} item or stack to push into `stack`
      */
     function stack(target, item) {
         var h, i = target.length;
@@ -42,8 +42,8 @@
 
     /**
     * $()
-    * @param  {*=}      item   CSS selector | DOM node(s) | fn to fire | anything
-    * @param  {Object=} root   node(s) from which to base selector queries
+    * @param {?(string|Node|Object|{length:number}|Function|*)=} item
+    * @param {?(Node|{length:number})=} root node(s) from which to base selector queries
     * @return {Dj}
     */  
     function dj(item, root) {
@@ -52,8 +52,8 @@
 
     /**
      * @constructor
-     * @param  {*=}      item   CSS selector | DOM node(s) | fn to fire | anything
-     * @param  {Object=} root   node(s) from which to base selector queries
+     * @param {?(string|Node|Object|{length:number}|Function|*)=} item
+     * @param {?(Node|{length:number})=} root node(s) from which to base selector queries
      * adapted from jQuery and ender
      */  
     function Dj(item, root) {
@@ -66,44 +66,41 @@
         typeof item == 'function' ? hook('closure')(item, root) : null != item && stack(this, item);
     }
 
-    // sync the prototypes
-    // re: github.com/ender-js/ender-js/pull/17
+    // Sync the prototypes
+    // http://github.com/ender-js/ender-js/pull/17
     dj['fn'] = dj.prototype = Dj.prototype;
 
     dj['fn']['$'] = dj; // reference to self
 
     /**
      * @deprecated Replace with `blood.twin`
-     * @link   github.com/ryanve/blood
-     * @link   stackoverflow.com/q/16594717/770127
+     * @link http://github.com/ryanve/blood
+     * @link http://stackoverflow.com/q/16594717/770127
      * @return {Object}
      */
     function resample() {
-        if (typeof blood != 'undefined')
-            return blood.twin.apply(this, arguments);
+        if (typeof blood != 'undefined') return blood.twin.apply(this, arguments);
         throw new Error('@deprecated @resample');
     }
 
     /**
      * Multi-purpose extdj/augmenter, with simple yet very useful options, called 
      * expand b/c many libs implement extend/augment methods, so this makes it easier 
-     * to integrate ( and drop preconceived notions about what it should do ). 
+     * to integrate (and drop preconceived notions about what it should do). 
      * Expand your mind ;]
-     *
-     * @param  {Object|Function}     receiver
-     * @param  {Object|Function}     supplier
-     * @param  {boolean=}            force     whether to overwrite existing props
-     *                                         true  ==> no typechecking is done.
-     *                                         false ==> receiver[n] must be null|undefined.
-     *                                                   supplier[n] must NOT be null|undefined.
-     * @param  {(boolean|Function)=}  check    whether supplier props must be owned (or a 
-     *                                         custom test, default: false)
+     * @param {Object|Function} receiver
+     * @param {Object|Function} supplier
+     * @param {boolean=} force whether to overwrite existing props
+     *   true => no typechecking is done.
+     *   false => receiver[n] must be null|undefined, supplier[n] must NOT be null|undefined
+     * @param {(boolean|Function)=} check whether supplier props must be owned (or a 
+     *   custom test, default: false)
      */
     function expand(receiver, supplier, force, check) {
         var n;
         if (null == receiver) throw new TypeError('@expand');
         if (null == supplier) return receiver;
-        force = force === true; // must be explicit
+        force = force === true;
         check = check === true ? owns : check;
         for (n in supplier) {
             if (force || null == receiver[n] && null != supplier[n]) {
@@ -117,10 +114,10 @@
 
     /**
      * Mixin owned enumerable props from the `supplier` into `this`.
-     * @this   {(Object|Function)}            is the receiver
-     * @param  {(Object|Function)}  supplier  is the supplier
-     * @param  {boolean=}           force     whether to overwrite existing props
-     * @param  {boolean=}           check     whether props must be owned (default: true)
+     * @this {(Object|Function)} receiver
+     * @param {(Object|Function)} supplier
+     * @param {boolean=} force whether to overwrite existing props
+     * @param {boolean=} check whether props must be owned (default: true)
      */
     function mixin(supplier, force, check) {
         if (this == globe) throw new Error('@this');
@@ -128,38 +125,34 @@
     }
 
     /**
-     * bridge()      Integrate applicable methods|objects into a host. Other 
-     *               types (number|string|undefined|boolean|null) are not bridged. 
-     *               `this` augments the receiver `r`. `bridge()` is designed for
-     *               merging jQueryish modules, thus `.fn` props bridge one level deep.
-     *
-     *               Methods|objects whose `.relay` property is set to `false` get
-     *               skipped. If the `.relay` property is a function, it is fired 
-     *               with `this` being the method|object and the 1st arg being the 
-     *               main scope (e.g. $ function) of the receiving api. This provides
-     *               a way for the method|object to be adapted to the receiving api.
-     *
-     *               If the `.relay` returns a truthy value (such as new func) then that 
-     *               value is transferred instead of the orig. If the relay returns `false` 
-     *               then the method|ob is skipped. If it returns any other falsey value 
-     *               then the transferred method will default back to the orig. So in effect, 
-     *               the `.relay` prop defaults to `true` and it is not necessary to define 
-     *               it for methods|obs that are to be transferred as is.
-     *       
-     * @this  {Object|Function}                supplier
-     * @param {Object|Function}         r      receiver
-     * @param {boolean=}                force  whether to overwrite existing props (default: false)
-     * @param {(Object|Function|null)=} $      the top-level of the host api (default: `r`)
-     *                                         For default behavior `$` should be omitted or set to 
-     *                                         `undefined`. This param allows you to bridge to a receiver, 
-     *                                         but relay methods based on a another host, for example 
-     *                                         `someModule.bridge({}, false, jQuery)`. Set `$` explicity
-     *                                         to `null` *only* if you want to communicate to relays that
-     *                                         there should be *no* main api.                                   
+     * Integrate applicable methods|objects into a host. Other 
+     * types (number|string|undefined|boolean|null) are not bridged. 
+     * `this` augments the receiver `r`. `bridge()` is designed for
+     * merging jQueryish modules, thus `.fn` props bridge one level deep.
+     * Methods|objects whose `.relay` property is set to `false` get
+     * skipped. If the `.relay` property is a function, it is fired 
+     * with `this` being the method|object and the 1st arg being the 
+     * main scope (e.g. $ function) of the receiving api. This provides
+     * a way for the method|object to be adapted to the receiving api.
+     * If the `.relay` returns a truthy value (such as new func) then that 
+     * value is transferred instead of the orig. If the relay returns `false` 
+     * then the method|ob is skipped. If it returns any other falsey value 
+     * then the transferred method will default back to the orig. So in effect, 
+     * the `.relay` prop defaults to `true` and it is not necessary to define 
+     * it for methods|obs that are to be transferred as is.
+     * @link https://github.com/ryanve/submix
+     * @this {Object|Function} supplier
+     * @param {Object|Function} r receiver
+     * @param {boolean=} force  whether to overwrite existing props (default: false)
+     * @param {(Object|Function|null)=} $ the top-level of the host api (default: `r`)
+     *   For default behavior `$` should be omitted or set to `undefined`. This param 
+     *   allows you to bridge to a receiver, but relay methods based on a another host,
+     *   for example `someModule.bridge({}, false, jQuery)`. Set `$` explicity to `null`
+     *    *only* if you want to communicate to relays that there should be *no* main api.                                   
      */
     function bridge(r, force, $) {
         var v, k, relay, s = this; // s is the supplier
-        if (!r || s === globe) { return; }
+        if (!r || s === globe) return;
         force = true === force; // require explicit true to force
         $ = typeof $ == 'function' || typeof $ == 'object' ? $ : r; // allow null
 
@@ -180,15 +173,16 @@
                         // This passes the essentials:
                         relay = relay.call(v, $, r[k]);
                     }
-                    if (relay !== false) {// Provides a way to bypass non-agnostic props.
+                    if (relay !== false) {
+                        // The check above provides a way to bypass non-agnostic props.
                         // Transfer the value. Default to the orig supplier value:
                         r[k] = relay || v;
                     }
                 }
             }
         }
-        return r; // receiver
-    }// bridge
+        return r;
+    }
     
     // signify that this bridge() is module agnostic
     bridge['relay'] = true;
@@ -202,9 +196,9 @@
           
         /** 
          * Method for setting/getting hooks
-         * @param {*=}  k     key
-         * @param {*=}  v     value
-         * @param {*=}  guard
+         * @param {*=} k key
+         * @param {*=} v value
+         * @param {*=} guard
          */
         function hook(k, v, guard) {
             var n, bool, prefix; 
